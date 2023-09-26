08d657d4a1509a01410a4de2631b3310626fa196
Vansh=Narang <vanshnarang49906@gmail.com>
2023-09-26 20:56:34 +0530

    Some pages made

---
+++ src/Pages/Home.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { FaArrowRight } from "react-icons/fa"
+function Home() {
+    return (
+        <div>
+            {/* Section 1  */}
+            <div>
+                <Link to="signup">
+                    <div>
+                        <div>
+                            <p>Become an instructor</p>
+                            <FaArrowRight />
+                        </div>
+                    </div>
+                </Link>
+            </div>
+            {/* Section 2  */}
+            {/* Section 3  */}
+            {/* Footer 4  */}
+
+        </div>
+    )
+}
+
+export default Home
