bdf1dda756327294bb9568dc7f7aba66d8d60865
Vansh=Narang <vanshnarang49906@gmail.com>
2023-09-27 20:06:38 +0530

    Added assets and codeblocks

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -3,6 +3,9 @@ import { Link } from "react-router-dom"
 import { FaArrowRight } from "react-icons/fa"
 import HighlightText from '../components/core/HomePage/HighlightText'
 import CTAButton from '../components/core/HomePage/Button'
+import Banner from "../assets/App.mp4"
+// import BannerI from "../assets/banner.jpg"
+import CodeBlocks from '../components/core/HomePage/CodeBlocks'
 function Home() {
 
     return (
@@ -38,6 +41,36 @@ function Home() {
                 <CTAButton active={false} linkto={"/login"}>
                     Book a Demo
                 </CTAButton>
+            </div>
+
+            <div className='mx-3 my-12 shadow-blue-200'>
+                <video muted loop autoPlay>
+                    <source src={Banner} type="video/mp4"></source>
+                </video>
+            </div>
+
+            <div>
+                <CodeBlocks
+                    position={"lg:flex-row"}
+                    heading={
+                        <div className='text-4xl font-semibold'>
+                            Unlock Your <HighlightText text={"Coding potential"} />
+                            with our online courses
+                        </div>
+                    }
+                    subheading={
+                        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, "
+                    }
+                    ctabtn1={
+                        {
+                            btnText: "Try it Yourself",
+                            linkto: "/signup",
+                            active: true
+                        }
+                    }
+
+
+                />
             </div>
             {/* Section 2  */}
             {/* Section 3  */}
