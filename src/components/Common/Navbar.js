d472783f1d671aa92622ead83f490a5531a267ee
Vansh=Narang <vanshnarang49906@gmail.com>
2023-10-02 20:32:00 +0530

    Updated Navbar according to the user rcvd

--- src/components/Common/Navbar.js
+++ src/components/Common/Navbar.js
@@ -3,8 +3,18 @@ import { Link, NavLink, matchPath } from "react-router-dom"
 import Logo from "../../assets/asset 0.png"
 import { NavbarLinks } from "../../data/navbar-links"
 import { useLocation } from 'react-router-dom'
-
+import { useSelector } from "react-redux"
+import { AiOutlineShoppingCart } from "react-icons/ai"
+import ProfileDropDown from "../core/Auth/ProfileDropDown"
 function Navbar() {
+
+    //fetch all the state from the reducer to see whether the user is logged in or not
+    const { token } = useSelector((state) => state.auth);
+    const { user } = useSelector((state) => state.profile);
+    const { totalItems } = useSelector((state) => state.cart);
+
+
+
     const location = useLocation()
     const matchRoute = (route) => {
         //match the path of url with the path coming from the navnbar links array
@@ -25,7 +35,11 @@ function Navbar() {
                                 return (
                                     <li key={index}>
                                         {
-                                            element.title === "Catalog" ? (<div></div>) : (
+                                            element.title === "Catalog" ? (
+                                                <div>
+
+                                                </div>
+                                            ) : (
                                                 <Link to={element?.path}>
                                                     <p className={`${matchRoute(element?.path) ? "text-yellow-25" : "text-richblack-100"}`}>
                                                         {element.title}
@@ -41,7 +55,44 @@ function Navbar() {
                 </nav>
                 {/* Login /signup */}
                 <div className='flex gap-x-4 items-center'>
-                    
+                    {
+                        //cart the data (if user is present)
+                        user && user?.accountType != "Instructor" && (
+                            <Link to="/dashboard/cart" className='relative' >
+                                <AiOutlineShoppingCart />
+                                {
+                                    totalItems > 0 && (
+                                        <span>
+                                            {totalItems}
+                                        </span>
+                                    )
+                                }
+                            </Link>
+                        )
+                    }
+                    {
+                        token === null && (
+                            <Link to="/login">
+                                <button className='border border-richblack-700 bg-richblack-800 px-[12px] py-[8px]
+                                text-richblack-100 rounded-md'>
+                                    Log In
+                                </button>
+                            </Link>
+                        )
+                    }
+                    {
+                        token === null && (
+                            <Link to="/signup">
+                                <button className='border border-richblack-700 bg-richblack-800 px-[12px] py-[8px]
+                                text-richblack-100 rounded-md'>
+                                    Sign Up
+                                </button>
+                            </Link>
+                        )
+                    }
+                    {
+                        token !== null && <ProfileDropDown />
+                    }
                 </div>
             </div>
 
