01ae93134c0b5bcb2f4dd2effb569a6d7d079463
Vansh=Narang <vanshnarang49906@gmail.com>
2023-10-02 12:08:18 +0530

    Auth slice completed

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+import rootReducer from "./reducer"
+
+
 
 const store = configureStore({
   reducer: rootReducer
---
+++ src/slices/authSlice.js
@@ -0,0 +1,30 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    //local storage se get kro value
+
+
+    //When we login the 
+    //application for the first time
+    // we will get the token we will store it
+    // in local storage then in that local storage 
+    //we have the access token if we leave the application 
+    //the local storage will have our access token
+    // for accessing in the future
+
+    //if get the token from local storage then parse else set it to null
+    token: localStorage.getItem("token") ? JSON.parse(localStorage.getItem("token")) : null,
+};
+
+const authSlice = createSlice({
+    name: "auth",
+    initialState: initialState,
+    reducers: {
+        setToken(state, value) {
+            state.token = value.payload;
+        },
+    },
+});
+
+export const { setToken } = authSlice.actions;
+export default authSlice.reducer;
