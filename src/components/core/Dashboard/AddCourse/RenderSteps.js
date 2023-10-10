9cc7a50f2e6a322d94e31d30e1cf43f1c8933aef
Vansh=Narang <vanshnarang49906@gmail.com>
2023-10-10 19:59:25 +0530

    More Slices added

--- src/components/core/Dashboard/AddCourse/RenderSteps.js
+++ src/components/core/Dashboard/AddCourse/RenderSteps.js
@@ -1,8 +1,24 @@
 import React from 'react'
 
 function RenderSteps() {
+    const steps = [
+        {
+            id: 1,
+            title: "Course Information"
+        },
+        {
+            id: 2,
+            title: "Course Builder"
+        },
+        {
+            id: 3,
+            title: "Publish"
+        }
+    ]
     return (
-        <div>RenderSteps</div>
+        <div>
+            
+        </div>
     )
 }
 
--- src/reducer/index.js
+++ src/reducer/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from "@reduxjs/toolkit";
 import authReducer from "../slices/authSlice"
 import profileReducer from "../slices/profileSlice"
 import cartReducer from "../slices/cartSlice"
-
+import courseReducer from "../slices/CourseSlice"
+import viewCourseReducer from "../slices/viewCourseSlice"
 const rootReducer = combineReducers({
     auth: authReducer,
     profile: profileReducer,
-    cart: cartReducer
-
+    cart: cartReducer,
+    course: courseReducer,
+    viewCourse: viewCourseReducer,
 })
 export default rootReducer
