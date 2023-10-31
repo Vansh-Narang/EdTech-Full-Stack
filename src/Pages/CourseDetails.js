7a91e674bdc8072d0bbf08fa585a594fc656b15d
Vansh=Narang <vanshnarang49906@gmail.com>
2023-10-31 11:40:37 +0530

    Buy course done

--- src/Pages/CourseDetails.js
+++ src/Pages/CourseDetails.js
@@ -8,6 +8,7 @@ import ConfirmationModal from "../components/Common/ConfirmationModal"
 import Error from "../Pages/Error"
 import RatingStars from "../components/Common/RatingStars"
 import CourseDetailsCard from "../components/core/Course/CourseDetailsCard"
+import { formattedDate } from "../utils/formattedDate"
 const CourseDetails = () => {
 
     const { user } = useSelector((state) => state.profile)
@@ -17,15 +18,19 @@ const CourseDetails = () => {
     const { courseId } = useParams()
     const { loading } = useSelector((state) => state.profile)
     const { paymentLoading } = useSelector((state) => state.course)
-
+    //get rating wala function
+    const [avgReviewCount, setavgReviewCount] = useState(0);
     const [response, setResponse] = useState(null)
     const [courseData, setCourseData] = useState(null)
     const [confirmationModal, setConfirmationModal] = useState(null)
+    //means empty array (saare close by default)
+    const [isActive, setIsActive] = useState(Array(0))
+
     useEffect(() => {
         //saare course ki details backend me hai function
         const getCourseFullDetails = async () => {
             try {
-                const result = await fetchCourseDetails("653e88e53f37930a20db2c38");
+                const result = await fetchCourseDetails(courseId);
                 console.log("Result: " + result)
                 setCourseData(result)
             } catch (error) {
@@ -35,8 +40,7 @@ const CourseDetails = () => {
         getCourseFullDetails();
     }, [courseId])
 
-    //get rating wala function
-    const [avgReviewCount, setavgReviewCount] = useState(0);
+
     useEffect(() => {
         const count = GetAvgRating(courseData?.data?.courseDetails.ratingAndReviews);
         setavgReviewCount(count);
@@ -54,7 +58,7 @@ const CourseDetails = () => {
 
     //to buy update
     const handleBuyCourse = () => {
-        // console.log("hellooooooooooooo")
+        console.log("hellooooooooooooo")
         if (token) {
             buyCourse(token, [courseId], user, navigate, dispatch)
             return;
@@ -97,8 +101,6 @@ const CourseDetails = () => {
         createdAt
     } = courseData.data?.courseDetails
 
-    //means empty array (saare close by default)
-    const [isActive, setIsActive] = useState(Array(0))
 
 
     const handleClick = (id) => {
@@ -118,21 +120,24 @@ const CourseDetails = () => {
                 <div className='text-white flex gap-x-2'>
                     <span> {avgReviewCount}</span>
                     <RatingStars Review_Count={avgReviewCount} Star_Size={24} />
-                    <span>{`(${ratingAndReviews.length} reviews)`}</span>
-                    <span>{`(${studentsEnrolled.length} students Enrolled)`}</span>
+                    <button className='text-white' onClick={handleBuyCourse}>
+                        Buy Now
+                    </button>
+                    {/* <span>{`(${ratingAndReviews.length} reviews)`}</span>
+                    <span>{`(${studentsEnrolled.length} students Enrolled)`}</span> */}
                 </div>
-                <div>
+                {/* <div>
                     <p>Created by {`${instructor.firstName}`}</p>
                 </div>
                 <div className='flex gap-x-3'>
                     <p>
-                        Created at {formatDate(createdAt)}
+                        Created at {formattedDate(createdAt)}
                     </p>
                     <p>
                         {" "} English
                     </p>
-                </div>
-                <div>
+                </div> */}
+                {/* <div>
                     <CourseDetailsCard
                         course={courseData?.data?.courseDetails}
                         setConfirmationModal={setConfirmationModal}
@@ -144,9 +149,9 @@ const CourseDetails = () => {
                     <div>
                         {whatYouWillLearn}
                     </div>
-                </div>
+                </div> */}
             </div>
-            <div>
+            {/* <div>
                 <div>
                     <p>Course Content :</p>
                 </div>
@@ -160,7 +165,7 @@ const CourseDetails = () => {
                             {courseData?.data?.totalDuration} total Length
                         </span>
                         <div>
-                            {/* set is active will be empty if clicked on collapse all sections as it will remove them from active state */}
+                            set is active will be empty if clicked on collapse all sections as it will remove them from active state
                             <button onClick={() => setIsActive([])}>
                                 Collapse all sections
                             </button>
@@ -169,7 +174,7 @@ const CourseDetails = () => {
 
 
                 </div>
-            </div>
+            </div> */}
             {confirmationModal && <ConfirmationModal modalData={confirmationModal} />}
         </div>
     )
